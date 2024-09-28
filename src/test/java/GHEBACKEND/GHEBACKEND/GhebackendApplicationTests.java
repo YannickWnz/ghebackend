4664ce72def5f7fac0aa6bebd5e6845d10503735
YannickWnz <yannickwnz@gmail.com>
2024-09-28 11:36:27 +0100

    first commit

---
+++ src/test/java/GHEBACKEND/GHEBACKEND/GhebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package GHEBACKEND.GHEBACKEND;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

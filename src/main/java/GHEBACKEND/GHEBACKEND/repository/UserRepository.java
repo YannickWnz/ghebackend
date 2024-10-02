ed5251444edde56bf218f73c5722d919aa5f0d13
YannickWnz <yannickwnz@gmail.com>
2024-10-02 10:47:34 +0100

    user repository interface

--- src/main/java/GHEBACKEND/GHEBACKEND/repository/UserRepository.java
+++ src/main/java/GHEBACKEND/GHEBACKEND/repository/UserRepository.java
@@ -3,13 +3,14 @@ package GHEBACKEND.GHEBACKEND.repository;
 import java.util.*;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 
 
 import GHEBACKEND.GHEBACKEND.model.User;
 
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
+public interface UserRepository extends JpaRepository<User, Long> {}

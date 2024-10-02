52c1dffa8976273ec57e97a831dc10bae55bfa8a
YannickWnz <yannickwnz@gmail.com>
2024-10-02 12:49:22 +0100

    implemented register user endpoint

--- src/main/java/GHEBACKEND/GHEBACKEND/controller/UserController.java
+++ src/main/java/GHEBACKEND/GHEBACKEND/controller/UserController.java
@@ -7,25 +7,34 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
-
-
+import org.springframework.web.bind.annotation.GetMapping;
 
 import GHEBACKEND.GHEBACKEND.model.User;
 import GHEBACKEND.GHEBACKEND.service.UserService;
 
 @RestController
-@RequestMapping("/api/auth")
+// @RequestMapping("/api/auth")
+// @RequestMapping("/api/auth")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @PostMapping("/register")
+    // public User registerUser(@RequestBody User user) {
+    //     // userService.registerUser(user);
+    //     return user;
+    // }
     public ResponseEntity<?> registerUser(@RequestBody User user) {
         userService.registerUser(user);
         return ResponseEntity.ok("User successfully registered.");
-
     }
+
+    // @GetMapping
+    // @PostMapping("/login")
+    // public String returnSmth() {
+    //     return "trying ....";
+    // }
 
 
 }

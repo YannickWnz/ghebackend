f8c1b7d75790bae6507d4fbbd800c8b8a85bff80
YannickWnz <yannickwnz@gmail.com>
2024-10-03 13:14:51 +0100

    jwt auth filter class implemented

---
+++ src/main/java/GHEBACKEND/GHEBACKEND/security/JwtAuthFilter.java
@@ -0,0 +1,49 @@
+package GHEBACKEND.GHEBACKEND.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.jsonwebtoken.io.IOException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+    HttpServletResponse response,
+    FilterChain filterChain) throws IOException {
+
+        try {
+            String jwt = parseJwt(request);
+            if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
+                String username = jwtUtils.getUsernameFromJwtToken(jwt);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(username, null, null);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+            filterChain.doFilter(request, response);
+        } catch (IOException e) {
+            throw e;  // If you can't handle it, rethrow
+        } 
+        catch (ServletException | java.io.IOException e) {
+            // Catch other general exceptions, including ServletException
+            throw new RuntimeException("Exception in filtering the request", e);
+        }
+    }
+
+    private String parseJwt(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+        if (headerAuth != null && headerAuth.startsWith("Bearer ")) {
+            return headerAuth.substring(7);
+        }
+        return null;
+    }
+
+}

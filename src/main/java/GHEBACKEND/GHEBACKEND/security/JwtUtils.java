17c78219755644be5ca636e843ba7330c5e667bb
YannickWnz <yannickwnz@gmail.com>
2024-10-04 11:41:43 +0100

    jwt utils working scripts

--- src/main/java/GHEBACKEND/GHEBACKEND/security/JwtUtils.java
+++ src/main/java/GHEBACKEND/GHEBACKEND/security/JwtUtils.java
@@ -4,20 +4,28 @@ import io.jsonwebtoken.*;
 import org.springframework.stereotype.Component;
 import java.util.*;
 
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+
 @Component
 public class JwtUtils {
 
-    private final String secretKey = "secret-key";
+    private final String secretKey = "13f44524ad70c6a921e1a4741f63f8f9aafa3d6b6c67212be69f8197890b14bb";
     private final long jwtExpirations = 86400;
 
-    @SuppressWarnings("deprecation")
+    // @SuppressWarnings("deprecation")
     public String generateJwtToken(String username) {
+
+        // Generate key from secret string 
+        Key key = new SecretKeySpec(secretKey.getBytes(), SignatureAlgorithm.HS256.getJcaName());
+
 
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date((new Date()).getTime() + jwtExpirations))
-                .signWith(SignatureAlgorithm.HS512, secretKey)
+                .signWith(key, SignatureAlgorithm.HS256) // Non-deprecated method
+                // .signWith(SignatureAlgorithm.HS512, secretKey)
                 .compact();
     }
 

f9f9ffbdadd7f54960222397868c9414d8c4f7ba
YannickWnz <yannickwnz@gmail.com>
2024-10-04 11:58:16 +0100

    login function added

--- src/main/java/GHEBACKEND/GHEBACKEND/controller/UserController.java
+++ src/main/java/GHEBACKEND/GHEBACKEND/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -46,6 +47,18 @@ public class UserController {
         String token = userService.loginUser(user.getCode(), user.getPassword());
         // String token = jwtUtils.generateJwtToken(user.getNom());
         return ResponseEntity.ok("User successfully registered." + token);
+    }
+
+    @PostMapping("/login")
+    // public ResponseEntity<?> loginUser(@RequestParam String code, @RequestParam String password) {
+    public ResponseEntity<?> loginUser(@RequestBody User user) {
+        String token = userService.loginUser(user.getCode(), user.getPassword());
+        if (token != null) {
+            return ResponseEntity.ok("User successfully logged in" + token);
+        } else {
+            return ResponseEntity.status(401).body("Invalid credentials");
+        }
+        // return ResponseEntity.badRequest().body("Invalid credentials");
     }
 
     // @GetMapping

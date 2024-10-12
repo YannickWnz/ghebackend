a42641ca97ce9360f97927cc17d35bed9eacbffd
YannickWnz <yannickwnz@gmail.com>
2024-10-12 11:34:35 +0100

    promotion ref data register function

--- src/main/java/GHEBACKEND/GHEBACKEND/service/PromotionService.java
+++ src/main/java/GHEBACKEND/GHEBACKEND/service/PromotionService.java
@@ -1,5 +1,31 @@
 package GHEBACKEND.GHEBACKEND.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import GHEBACKEND.GHEBACKEND.model.Promotion;
+
+@Service
 public class PromotionService {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public void addPromotion(Promotion promotion) {
+
+        String insertPromotionQuery = "INSERT INTO T_PROMOTIO (PRO_CODE, PRO_LIB, PRO_CREER_PAR, PRO_VERSION) VALUES (?, ?, ?, ?)";
+
+        // running insert query
+        jdbcTemplate.update(
+                insertPromotionQuery, 
+                promotion.getProCode(), 
+                promotion.getProLib(), 
+                promotion.getCreerPar(), 
+                promotion.getProVersion()
+            );
+
+    }
+    
 
 }

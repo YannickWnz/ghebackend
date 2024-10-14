22db791cbf470ff1d3f22fd5752521297409df7c
YannickWnz <yannickwnz@gmail.com>
2024-10-14 11:47:39 +0100

    add annee academique controller method

--- src/main/java/GHEBACKEND/GHEBACKEND/controller/AnneeAcademiqueController.java
+++ src/main/java/GHEBACKEND/GHEBACKEND/controller/AnneeAcademiqueController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import GHEBACKEND.GHEBACKEND.model.AnneeAcademique;
@@ -22,6 +24,14 @@ public class AnneeAcademiqueController {
     @GetMapping("/api/getAnneeAcademique")
     public ResponseEntity<List<AnneeAcademique>> getAllAnneeAcademique() {
         return new ResponseEntity<>(anneeAcademiqueService.getAllAnneeAcademique(), HttpStatus.OK);
+    }
+
+    @PostMapping("/api/anneeAcademique")
+    public ResponseEntity<?> addAnneAcademique(@RequestBody AnneeAcademique anneeAcademique) {
+
+        anneeAcademiqueService.addAnneAcademique(anneeAcademique);
+
+        return ResponseEntity.ok("Annee Academique successfully created");
     }
 
 }

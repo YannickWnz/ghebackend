d848c90ce400d84555622890bc62335f87ae3b7a
YannickWnz <yannickwnz@gmail.com>
2024-10-14 09:34:00 +0100

    annee ac model complete

--- src/main/java/GHEBACKEND/GHEBACKEND/model/AnneeAcademique.java
+++ src/main/java/GHEBACKEND/GHEBACKEND/model/AnneeAcademique.java
@@ -1,5 +1,7 @@
 package GHEBACKEND.GHEBACKEND.model;
 
+import org.springframework.web.util.UriBuilder;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -24,6 +26,54 @@ public class AnneeAcademique {
     
     @Column(name="AAC_VERSION")
     private int aac_version;
+
+    // getters
+    
+    public int getAacCode() {
+        return aac_code;
+    }
+    
+    public String getAacLib() {
+        return aac_lib;
+    }
+
+    public String getAacCreerPar() {
+        return aac_creer_par;
+    }
+
+    public String getAacModifierPar() {
+        return aac_modifier_par;
+    }
+
+    public int getAacVersion() {
+        return aac_version;
+    }
+
+    
+    // setters
+    public void setAacCode(int aac_code) {
+        this.aac_code = aac_code;
+    }
+
+    public void setAacLib(String aac_lib) {
+        this.aac_lib = aac_lib;
+    }
+
+    public void setAacCreerPar(String aac_creer_par) {
+        this.aac_creer_par = aac_creer_par;
+    }
+
+    public void setAacModifierPar(String aac_modifier_par) {
+        this.aac_modifier_par = aac_modifier_par;
+    }
+
+    public void setAacVersion(int aac_version) {
+        this.aac_version = aac_version;
+    }
+    
+
+
+
 
     
 

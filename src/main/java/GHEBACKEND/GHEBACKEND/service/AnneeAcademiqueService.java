1126706f9e3aa7d66a183a5a4cf3e85bc5a37f56
YannickWnz <yannickwnz@gmail.com>
2024-10-14 11:47:12 +0100

    add aac service function

--- src/main/java/GHEBACKEND/GHEBACKEND/service/AnneeAcademiqueService.java
+++ src/main/java/GHEBACKEND/GHEBACKEND/service/AnneeAcademiqueService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import GHEBACKEND.GHEBACKEND.model.AnneeAcademique;
 import GHEBACKEND.GHEBACKEND.repository.AnneeAcademiqueRepo;
+import GHEBACKEND.GHEBACKEND.utils.CodeGenerator;
 
 @Service
 public class AnneeAcademiqueService {
@@ -18,8 +19,36 @@ public class AnneeAcademiqueService {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @Autowired
+    private CodeGenerator codeGenerator;
+
     public List<AnneeAcademique> getAllAnneeAcademique() {
         return anneeAcademiqueRepo.findAll();
+    }
+
+    public void addAnneAcademique(AnneeAcademique anneeAcademique) {
+
+        // get code from generateCode util method
+        int code = codeGenerator.generateCode("ANNEE_ACADEMIQUE");
+
+         // Set the unique aac code to the aac object
+         anneeAcademique.setAacCode(code);
+         
+         // Set default version to 1 
+         anneeAcademique.setAacVersion(1);
+
+
+         String insertAacQuery = "INSERT INTO T_ANNEE_ACADEMIQUE (AAC_CODE, AAC_LIB, AAC_CREER_PAR, AAC_STATUS, AAC_VERSION) VALUES (?, ?, ?, ?, ?)";
+ 
+         // running insert query
+         jdbcTemplate.update(
+                insertAacQuery, 
+                anneeAcademique.getAacCode(),
+                anneeAcademique.getAacLib(),
+                anneeAcademique.getAacCreerPar(),
+                anneeAcademique.getAacStatus(),
+                anneeAcademique.getAacVersion()
+             );
     }
 
 }

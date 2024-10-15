922bb4191c019f397cea2989bce70d4c2d6002fc
YannickWnz <yannickwnz@gmail.com>
2024-10-15 10:04:48 +0100

    update aac controller method implemented

--- src/main/java/GHEBACKEND/GHEBACKEND/controller/AnneeAcademiqueController.java
+++ src/main/java/GHEBACKEND/GHEBACKEND/controller/AnneeAcademiqueController.java
@@ -7,11 +7,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import GHEBACKEND.GHEBACKEND.model.AnneeAcademique;
+import GHEBACKEND.GHEBACKEND.model.Promotion;
 import GHEBACKEND.GHEBACKEND.service.AnneeAcademiqueService;
 
 @CrossOrigin("http://localhost:3000")
@@ -28,10 +31,23 @@ public class AnneeAcademiqueController {
 
     @PostMapping("/api/anneeAcademique")
     public ResponseEntity<?> addAnneAcademique(@RequestBody AnneeAcademique anneeAcademique) {
-
+        
         anneeAcademiqueService.addAnneAcademique(anneeAcademique);
-
+        
         return ResponseEntity.ok("Annee Academique successfully created");
+    }
+    
+    @PutMapping("/api/anneeAcademique/{aacCode}")
+    public ResponseEntity<?> updateAnneeAcademique(@PathVariable int aacCode, @RequestBody AnneeAcademique updatedAacData) {
+
+        anneeAcademiqueService.updateAnneeAcademique(
+                aacCode, 
+                updatedAacData.getAacLib(), 
+                updatedAacData.getAacModifierPar(), 
+                updatedAacData.getAacStatus()
+            );
+
+        return ResponseEntity.ok("Data successfully updated");
     }
 
 }

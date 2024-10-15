1dc1f0f0baae476f632026564d077b2f8555a0f7
YannickWnz <yannickwnz@gmail.com>
2024-10-15 13:21:15 +0100

    new functions implemented

--- src/main/java/GHEBACKEND/GHEBACKEND/utils/UtilityMethods.java
+++ src/main/java/GHEBACKEND/GHEBACKEND/utils/UtilityMethods.java
@@ -1,8 +1,15 @@
 package GHEBACKEND.GHEBACKEND.utils;
 
+import java.util.Date;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 
 @Component
 public class UtilityMethods {
@@ -17,5 +24,55 @@ public class UtilityMethods {
         jdbcTemplate.update(deleteQuery, code);
 
     }
+
+    // Method generatrice de codes
+    public Integer codeGenerator(String codeName, String tableName) { 
+        // code par default si aucune valeur n'existe dans la base de donnees
+        int defaultCode = 1001;
+    
+        // requete qui recupere la valeur max existante dans la base
+        String query = "SELECT MAX(" + codeName + ") FROM " + tableName;
+    
+        // Querying db
+        Integer maxCode = jdbcTemplate.queryForObject(query, Integer.class);
+    
+        // si aucune valeur n'est trouver ... defaultCode is returned ... else le max est returned apres incrementation by 1 
+        return (maxCode != null) ? (maxCode + 1) : defaultCode;
+    }
+
+    // utility methods qui ajoute les donnees aux tables qui ont les champs suivant (code, lib, creerPar, modifierPar, dateCreate, version)
+    public void addDonneesRef(String lib, String creerPar, String codeName, String tableName) {
+
+
+        Integer Code = codeGenerator("FIL_CODE", "T_FILIERE");
+
+        Integer version = 1;
+
+        String query = "INSERT INTO " + tableName + " (FIL_CODE, FIL_LIB, FIL_CREER_PAR, FIL_VERSION) VALUES (?, ?, ?, ?)";
+
+        jdbcTemplate.update(
+            query,
+            Code,
+            lib,
+            creerPar,
+            version
+        );
+
+    }
+
+    // method qui se charge de la recuperation de la date et heure actuelle
+    public String getCurrentDateTime() {
+        // Get the current date and time
+        LocalDateTime currentDateTime = LocalDateTime.now();
+
+        // Format the date and time
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        // DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        // Return the formatted date and time as a string
+        return currentDateTime.format(formatter);
+    }
+
+
 
 }

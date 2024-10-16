72f736f277dde088f8a77439be140af6471a0062
YannickWnz <yannickwnz@gmail.com>
2024-10-16 12:23:52 +0100

    update function

--- src/main/java/GHEBACKEND/GHEBACKEND/service/DonneesReferentielles/NiveauService.java
+++ src/main/java/GHEBACKEND/GHEBACKEND/service/DonneesReferentielles/NiveauService.java
@@ -1,8 +1,11 @@
 package GHEBACKEND.GHEBACKEND.service.DonneesReferentielles;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import GHEBACKEND.GHEBACKEND.model.DonneesReferentielles.Niveau;
 import GHEBACKEND.GHEBACKEND.repository.DonneesReferentielles.NiveauRepo;
@@ -20,24 +23,28 @@ public class NiveauService {
     @Autowired
     private NiveauRepo niveauRepo;
 
-    public void addNewNiveau(Niveau niveau) {
+    public List<Niveau> getAllNiveau() {
 
-        // utilityMethods.addDonneesRef(niveau.getNivLib(), niveau.getNivCreerPar(), "NIV_CODE", "T_NIVEAU");
+        return niveauRepo.findAll();
+    }
+
+    @Transactional
+    public void addNewNiveau(Niveau niveau) {
 
         int code = utilityMethods.codeGenerator("NIV_CODE", "T_NIVEAU");
 
         Integer defaultVersion = 1;
 
-        String insertPromotionQuery = "INSERT INTO T_NIVEAU (NIV_CODE, NIV_LIB, NIV_CREER_PAR, NIV_VERSION) VALUES (?, ?, ?, ?)";
+        niveauRepo.insertNewNiveauData(code, niveau.getNivLib(), niveau.getNivCreerPar(), defaultVersion);
 
-        // running insert query
-        jdbcTemplate.update(
-                insertPromotionQuery, 
-                code, 
-                niveau.getNivLib(), 
-                niveau.getNivCreerPar(),
-                defaultVersion
-            );
+    }
+
+    @Transactional
+    public void updateNiveauData(int nivCode, Niveau niveau) {
+
+        int newVersion = utilityMethods.getCurrentVersion(nivCode, "NIV_CODE", "T_NIVEAU", "NIV_VERSION") + 1;
+
+        niveauRepo.updateNiveauData(nivCode, niveau.getNivLib(), niveau.getNivModifierPar(), newVersion);
 
     }
 

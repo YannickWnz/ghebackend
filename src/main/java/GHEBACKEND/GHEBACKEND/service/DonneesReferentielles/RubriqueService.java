b618fa6ae136782fe30897b4a73e7df560d489e6
YannickWnz <yannickwnz@gmail.com>
2024-10-17 14:30:23 +0100

    rubrique service implemented

---
+++ src/main/java/GHEBACKEND/GHEBACKEND/service/DonneesReferentielles/RubriqueService.java
@@ -0,0 +1,64 @@
+package GHEBACKEND.GHEBACKEND.service.DonneesReferentielles;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import GHEBACKEND.GHEBACKEND.model.Promotion;
+import GHEBACKEND.GHEBACKEND.model.DonneesReferentielles.RubriqueModel;
+import GHEBACKEND.GHEBACKEND.repository.DonneesReferentielles.RubriqueRepo;
+import GHEBACKEND.GHEBACKEND.utils.UtilityMethods;
+
+@Service
+public class RubriqueService {
+
+    @Autowired
+    private RubriqueRepo rubriqueRepo;
+
+    @Autowired
+    private UtilityMethods utilityMethods;
+
+    public List<RubriqueModel> getAllRubrique() {
+        return rubriqueRepo.findAll();
+    }
+
+    public void addNewRubrique(RubriqueModel rubriqueModel) {
+
+        Integer code = utilityMethods.codeGenerator("RUB_CODE", "T_RUBRIQUE");
+
+         // version definie sur 1 par default a la creation
+         Integer version = 1;
+        
+         // getting datetime from utilityMethod class
+         String currentDateTime = utilityMethods.getCurrentDateTime();
+ 
+ 
+         rubriqueModel.setRubCode(code);
+         rubriqueModel.setRubVersion(version);
+         rubriqueModel.setRubDateCreation(currentDateTime);
+
+         rubriqueRepo.save(rubriqueModel);
+
+    }
+
+    public void updateRubriqueData(Integer code, RubriqueModel rubriqueModel) {
+
+        Integer newVersion = utilityMethods.getCurrentVersion(code, "RUB_CODE", "T_RUBRIQUE", "RUB_VERSION") + 1;
+
+        // find data before update else throw error
+        RubriqueModel existingRubriqueData = rubriqueRepo.findById(code)
+        .orElseThrow(() -> new IllegalArgumentException("Could not find data with given code"));
+        
+        // set new data then save ...
+            existingRubriqueData.setRubLib(rubriqueModel.getRubLib());
+            existingRubriqueData.setRubMontant(rubriqueModel.getRubMontant());
+            existingRubriqueData.setRubFraisUnique(rubriqueModel.getRubFraisUnique());
+            existingRubriqueData.setRubModifierPar(rubriqueModel.getRubModifierPar());
+            existingRubriqueData.setRubVersion(newVersion);
+
+            rubriqueRepo.save(existingRubriqueData);
+
+    }
+
+}

abcfc077b455a6332446eb5a2b8dba154934aa5f
YannickWnz <yannickwnz@gmail.com>
2024-10-17 15:50:14 +0100

    annee ac class controller

---
+++ src/main/java/GHEBACKEND/GHEBACKEND/controller/DonneesReferentielles/AnneeAcademiqueController.java
@@ -0,0 +1,75 @@
+package GHEBACKEND.GHEBACKEND.controller.DonneesReferentielles;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import GHEBACKEND.GHEBACKEND.model.DonneesReferentielles.AnneeAcademique;
+import GHEBACKEND.GHEBACKEND.service.DonneesReferentielles.AnneeAcademiqueService;
+import GHEBACKEND.GHEBACKEND.utils.UtilityMethods;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+public class AnneeAcademiqueController {
+
+    @Autowired
+    private AnneeAcademiqueService anneeAcademiqueService;
+
+    @Autowired
+    private UtilityMethods utilityMethods;
+
+    @GetMapping("/api/anneeAcademique")
+    public ResponseEntity<List<AnneeAcademique>> getAllAnneeAcademique() {
+        return new ResponseEntity<>(anneeAcademiqueService.getAllAnneeAcademique(), HttpStatus.OK);
+    }
+
+    @PostMapping("/api/anneeAcademique")
+    public ResponseEntity<?> addAnneAcademique(@RequestBody AnneeAcademique anneeAcademique) {
+        
+        anneeAcademiqueService.addAnneAcademique(anneeAcademique);
+        
+        return ResponseEntity.ok("Annee Academique successfully created");
+    }
+    
+    @PutMapping("/api/anneeAcademique/{aacCode}")
+    public ResponseEntity<?> updateAnneeAcademique(@PathVariable int aacCode, @RequestBody AnneeAcademique updatedAacData) {
+
+        anneeAcademiqueService.updateAnneeAcademique(
+                aacCode, 
+                updatedAacData.getAacLib(), 
+                updatedAacData.getAacModifierPar(), 
+                updatedAacData.getAacStatus()
+            );
+
+        return ResponseEntity.ok("Data successfully updated");
+    }
+
+    // controller function qui se charge de la suppression
+    @DeleteMapping("/api/anneeAcademique/{aacCode}")
+    public ResponseEntity<String> deleteAnneeAcademique(@PathVariable Integer aacCode) {
+
+        try {
+
+            utilityMethods.deleteDonneesRef(aacCode, "T_ANNEE_ACADEMIQUE", "AAC_CODE");
+
+            // promotionService.deletePromoData(proCode);
+            return ResponseEntity.ok("Annee Academique delete successfully");
+
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
+    }
+
+
+}

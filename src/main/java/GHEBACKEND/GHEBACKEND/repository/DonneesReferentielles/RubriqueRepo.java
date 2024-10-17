190275a2b81f081f218d9102b955b8eaacb95005
YannickWnz <yannickwnz@gmail.com>
2024-10-17 14:30:50 +0100

    methods removed

--- src/main/java/GHEBACKEND/GHEBACKEND/repository/DonneesReferentielles/RubriqueRepo.java
+++ src/main/java/GHEBACKEND/GHEBACKEND/repository/DonneesReferentielles/RubriqueRepo.java
@@ -10,18 +10,4 @@ import org.springframework.stereotype.Repository;
 import GHEBACKEND.GHEBACKEND.model.DonneesReferentielles.RubriqueModel;
 
 @Repository
-public interface RubriqueRepo extends JpaRepository<RubriqueModel, Integer> {
-
-    @Modifying
-    @Query(value="UPDATE T_RUBRIQUE SET RUB_LIB = ?2, RUB_MODIFIER_PAR = ?3, RUB_VERSION = ?4 WHERE RUB_CODE = ?1)", nativeQuery=true)
-    void updateRubriqueLib(Integer code, String rubLib, String rubModifierPar, Integer rubVersion);
-    
-    @Modifying
-    @Query(value="UPDATE T_RUBRIQUE SET RUB_MONTANT = ?2, RUB_MODIFIER_PAR = ?3, RUB_VERSION = ?4 WHERE RUB_CODE = ?1)", nativeQuery=true)
-    void updateRubriqueMontant(Integer code, String rubMontant, String rubModifierPar, Integer rubVersion);
-    
-    @Modifying
-    @Query(value="UPDATE T_RUBRIQUE SET RUB_FRAIS_UNIQUE = ?2, RUB_MODIFIER_PAR = ?3, RUB_VERSION = ?4 WHERE RUB_CODE = ?1)", nativeQuery=true)
-    void updateRubriqueFraisUnique(Integer code, String rubFraisUnique, String rubModifierPar, Integer rubVersion);
-
-}
+public interface RubriqueRepo extends JpaRepository<RubriqueModel, Integer> {}

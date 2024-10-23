ea730057f3a356ae9ec1bdfa730e09630c8e906e
YannickWnz <yannickwnz@gmail.com>
2024-10-23 09:49:44 +0100

    get total data method

--- src/main/java/GHEBACKEND/GHEBACKEND/controller/DonneesReferentielles/MatiereController.java
+++ src/main/java/GHEBACKEND/GHEBACKEND/controller/DonneesReferentielles/MatiereController.java
@@ -2,24 +2,23 @@ package GHEBACKEND.GHEBACKEND.controller.DonneesReferentielles;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import GHEBACKEND.GHEBACKEND.model.DonneesReferentielles.MatiereModel;
 import GHEBACKEND.GHEBACKEND.service.DonneesReferentielles.MatiereService;
 import GHEBACKEND.GHEBACKEND.utils.UtilityMethods;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -30,6 +29,11 @@ public class MatiereController {
 
     @Autowired
     private UtilityMethods utilityMethods;
+
+    @GetMapping("/api/matiere/totalCount")
+    public Integer getTotalDataNumber() {
+        return matiereService.getTotalDataNumber("T_MATIERE");
+    }
 
     @GetMapping("/api/matiere")
     public List<MatiereModel> getAllMatiere() {

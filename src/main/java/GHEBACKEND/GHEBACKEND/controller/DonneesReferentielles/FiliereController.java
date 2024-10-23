805c3903a0711aaa885542e82c5145267e3b8908
YannickWnz <yannickwnz@gmail.com>
2024-10-23 09:49:09 +0100

    get total data method

--- src/main/java/GHEBACKEND/GHEBACKEND/controller/DonneesReferentielles/FiliereController.java
+++ src/main/java/GHEBACKEND/GHEBACKEND/controller/DonneesReferentielles/FiliereController.java
@@ -2,26 +2,23 @@ package GHEBACKEND.GHEBACKEND.controller.DonneesReferentielles;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import GHEBACKEND.GHEBACKEND.model.DonneesReferentielles.Filiere;
 import GHEBACKEND.GHEBACKEND.service.DonneesReferentielles.FiliereService;
 import GHEBACKEND.GHEBACKEND.utils.UtilityMethods;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-
-import GHEBACKEND.GHEBACKEND.repository.DonneesReferentielles.FiliereRepo;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -32,6 +29,11 @@ public class FiliereController {
 
     @Autowired 
     private UtilityMethods utilityMethods;
+
+    @GetMapping("/api/filiere/totalCount")
+    public Integer getTotalDataNumber() {
+        return filiereService.getTotalDataNumber("T_FILIERE");
+    }
 
     // creating filiere
     @PostMapping("/api/filiere")

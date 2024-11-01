0ca4f958e88ece2f96a72846677dfcf6239856ba
YannickWnz <yannickwnz@gmail.com>
2024-11-01 15:57:44 +0100

    emploi ref data CRUD

--- src/main/java/GHEBACKEND/GHEBACKEND/controller/DonneesReferentielles/EmploiController.java
+++ src/main/java/GHEBACKEND/GHEBACKEND/controller/DonneesReferentielles/EmploiController.java
@@ -1,12 +1,18 @@
 package GHEBACKEND.GHEBACKEND.controller.DonneesReferentielles;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +27,10 @@ public class EmploiController {
     @Autowired
     private EmploiService emploiService;
 
-    // @GetMapping("/api/emploi")
+    @GetMapping("/api/emploi")
+    public List<EmploiModel> getAllEmploiData() {
+        return emploiService.getAllEmploiData();
+    }
 
     @PostMapping("/api/emploi")
     public ResponseEntity<String> addNewEmploi(@RequestBody EmploiModel emploiModel) {
@@ -44,6 +53,50 @@ public class EmploiController {
             logger.error("Error while creating emploi: ", e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred while creating emploi.");
         }
+    }
+
+    @PutMapping("/api/emploi/{code}")
+    public ResponseEntity<String> updateEmploiData(@PathVariable Integer code, @RequestBody EmploiModel emploiModel) {
+
+        Logger logger = LoggerFactory.getLogger(this.getClass());
+
+        try {
+         
+            if(!UtilityMethods.validateInputString(emploiModel.getEmpLib(), 2, 100)) {
+                return new ResponseEntity<>("Invalid Emploi Lib format", HttpStatus.BAD_REQUEST);
+            }
+
+            emploiService.editEmploiData(code, emploiModel);
+
+            return ResponseEntity.ok("Emploi succesfully updated");
+            
+        } catch (Exception e) {
+            logger.error("Error while updated emploi: ", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred while updated emploi.");
+        }
+
+    }
+
+    @DeleteMapping("/api/emploi/{code}")
+    public ResponseEntity<String> deleteEmploiData(@PathVariable Integer code) {
+
+        Logger logger = LoggerFactory.getLogger(this.getClass());
+
+        try {
+         
+            // if(!UtilityMethods.validateInputString(emploiModel.getEmpLib(), 2, 100)) {
+            //     return new ResponseEntity<>("Invalid Emploi Lib format", HttpStatus.BAD_REQUEST);
+            // }
+
+            emploiService.deleteEmploiData(code);
+
+            return ResponseEntity.ok("Emploi succesfully deleted");
+            
+        } catch (Exception e) {
+            logger.error("Error while deleting emploi data: ", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred while deleted emploi data.");
+        }
+
     }
 
     
--- src/main/java/GHEBACKEND/GHEBACKEND/service/DonneesReferentielles/EmploiService.java
+++ src/main/java/GHEBACKEND/GHEBACKEND/service/DonneesReferentielles/EmploiService.java
@@ -1,5 +1,7 @@
 package GHEBACKEND.GHEBACKEND.service.DonneesReferentielles;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,16 @@ public class EmploiService {
     @Autowired
     private UtilityMethods utilityMethods;
 
+    // Function fetching emploi data
+    public List<EmploiModel> getAllEmploiData() {
+
+        List<EmploiModel> fetchedEmploiData = emploiRepo.findAll();
+
+        return fetchedEmploiData;
+
+    }
+
+    // Function creating new emploi data
     public void addNewEmploi(EmploiModel emploiModel) {
 
         // recuperation du code apres generation par la methode utilitaire
@@ -35,5 +47,37 @@ public class EmploiService {
         emploiRepo.save(emploiModel);
 
     }
+
+    // Function editing existing emploi data
+    public void editEmploiData(Integer code, EmploiModel emploiModel) {
+
+        // get current promotion version from repo function
+        Integer currentVersion = utilityMethods.getCurrentVersion(code, "EMP_CODE", "T_EMPLOI", "EMP_VERSION");
+        
+        // get a new version by increment current by 1
+        Integer newVersion = currentVersion + 1;
+
+        EmploiModel existingEmploiData = emploiRepo.findById(code)
+            .orElseThrow(() -> new IllegalArgumentException("Could not find data using code provided"));
+        
+        existingEmploiData.setEmpLib(emploiModel.getEmpLib());
+        existingEmploiData.setEmpModifierPar(emploiModel.getEmpModifierPar());
+        existingEmploiData.setEmpVersion(newVersion);
+
+        emploiRepo.save(existingEmploiData);
+
+    }
+
+    public void deleteEmploiData(Integer code) {
+
+        if(emploiRepo.existsById(code)) {
+            emploiRepo.deleteById(code);
+        } else {
+            throw new IllegalArgumentException("Could not find data using code provided");
+        }
+
+    }
+
+
 
 }

dfd0722d8a598612d86168c2b8f6f1c4495ac1b6
YannickWnz <yannickwnz@gmail.com>
2024-11-03 11:55:12 +0100

    lien crud complete

--- src/main/java/GHEBACKEND/GHEBACKEND/controller/DonneesReferentielles/LienController.java
+++ src/main/java/GHEBACKEND/GHEBACKEND/controller/DonneesReferentielles/LienController.java
@@ -1,6 +1,117 @@
 package GHEBACKEND.GHEBACKEND.controller.DonneesReferentielles;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import GHEBACKEND.GHEBACKEND.model.DonneesReferentielles.LienModel;
+import GHEBACKEND.GHEBACKEND.service.DonneesReferentielles.LienService;
+import GHEBACKEND.GHEBACKEND.utils.UtilityMethods;
+
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
-public class LienController {}
+public class LienController {
+
+    @Autowired
+    private LienService lienService;
+
+    @Autowired
+    private UtilityMethods utilityMethods;
+    
+    @GetMapping("/api/lien")
+    public List<LienModel> getAllLienData() {
+        return lienService.getAllLienData();
+    }
+
+    @GetMapping("/api/lien/totalCount")
+    public Integer getLienTotalCount() {
+        return lienService.getTotalDataNumber("T_LIEN");
+    }
+
+    @PostMapping("/api/lien")
+    public ResponseEntity<String> addNewLien(@RequestBody LienModel lienModel) {
+
+        Logger logger = LoggerFactory.getLogger(this.getClass());
+
+        try {
+
+         
+            if(!UtilityMethods.validateInputString(lienModel.getLieLib(), 2, 255)) {
+                return new ResponseEntity<>("Invalid Lien Lib format", HttpStatus.BAD_REQUEST);
+            }
+
+            lienService.addNewLien(lienModel);
+
+            return ResponseEntity.ok("Lien succesfully created");
+            
+        } catch (Exception e) {
+            logger.error("Error while creating lien: ", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred while creating lien.");
+        }
+        
+    }
+
+    @PutMapping("/api/lien/{code}")
+    public ResponseEntity<String> updateLienData(@PathVariable Integer code, @RequestBody LienModel lienModel) {
+
+        Logger logger = LoggerFactory.getLogger(this.getClass());
+
+        try {
+
+            
+            if(!UtilityMethods.validateInputCode(Integer.toString(code))) {
+                return new ResponseEntity<>("Invalid Lien Code format", HttpStatus.BAD_REQUEST);
+            }
+         
+            if(!UtilityMethods.validateInputString(lienModel.getLieLib(), 2, 255)) {
+                return new ResponseEntity<>("Invalid Lien Lib format", HttpStatus.BAD_REQUEST);
+            }
+
+            lienService.updateLienData(code, lienModel);
+
+            return ResponseEntity.ok("Lien succesfully updated");
+            
+        } catch (Exception e) {
+            logger.error("Error while updating lien: ", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred while updating lien.");
+        }
+        
+    }
+
+    @DeleteMapping("/api/lien/{code}")
+    public ResponseEntity<String> deleteLienData(@PathVariable Integer code) {
+
+        Logger logger = LoggerFactory.getLogger(this.getClass());
+
+        try {
+
+         
+            if(!UtilityMethods.validateInputCode(Integer.toString(code))) {
+                return new ResponseEntity<>("Invalid Lien Code format", HttpStatus.BAD_REQUEST);
+            }
+
+            lienService.deleteLienData(code);
+
+            return ResponseEntity.ok("Lien succesfully deleted");
+            
+        } catch (Exception e) {
+            logger.error("Error while deleting lien: ", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred while deleting lien.");
+        }
+
+    }
+
+
+}
--- src/main/java/GHEBACKEND/GHEBACKEND/service/DonneesReferentielles/LienService.java
+++ src/main/java/GHEBACKEND/GHEBACKEND/service/DonneesReferentielles/LienService.java
@@ -1,8 +1,82 @@
 package GHEBACKEND.GHEBACKEND.service.DonneesReferentielles;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import GHEBACKEND.GHEBACKEND.model.DonneesReferentielles.LienModel;
+import GHEBACKEND.GHEBACKEND.repository.DonneesReferentielles.LienRepo;
+import GHEBACKEND.GHEBACKEND.utils.UtilityMethods;
 
 @Service
 public class LienService {
+
+    @Autowired
+    private LienRepo lienRepo;
+
+    @Autowired
+    private UtilityMethods utilityMethods;
+
+    public List<LienModel> getAllLienData() {
+
+        List<LienModel> lienData = lienRepo.findAll();
+
+        return lienData;
+    
+    }
+
+    public Integer getTotalDataNumber(String tableName) {   
+        return utilityMethods.getTotalNumberOfDonneesRef(tableName);
+    }
+
+    public void addNewLien(LienModel lienModel) {
+
+        // recuperation du code apres generation par la methode utilitaire
+        Integer code = utilityMethods.codeGenerator("LIE_CODE", "T_LIEN");
+                
+        // version par default a la creation
+        Integer defaultVersion = 1;
+
+        // getting datetime from utilityMethod class
+        String currentDateTime = utilityMethods.getCurrentDateTime();
+
+        lienModel.setLieCode(code);
+        lienModel.setLieVersion(defaultVersion);
+        lienModel.setLieDateCreation(currentDateTime);
+
+        lienRepo.save(lienModel);
+
+
+    }
+
+    public void updateLienData(Integer code, LienModel lienModel) {
+
+        // get current promotion version from repo function
+        Integer currentVersion = utilityMethods.getCurrentVersion(code, "LIE_CODE", "T_LIEN", "LIE_VERSION");
+
+        // get a new version by increment current by 1
+        Integer newVersion = currentVersion + 1;
+        
+        LienModel existingLienData = lienRepo.findById(code)
+            .orElseThrow(() -> new IllegalArgumentException("Could not find data ..."));
+
+        existingLienData.setLieLib(lienModel.getLieLib());
+        existingLienData.setLieModifierPar(lienModel.getLieModifierPar());
+        existingLienData.setLieVersion(newVersion);
+
+        lienRepo.save(existingLienData);
+
+    }
+
+    public void deleteLienData(Integer code) {
+
+        if(lienRepo.existsById(code)) {
+            lienRepo.deleteById(code);
+        } else {
+            throw new IllegalArgumentException("Cound not find data with provided code ...");
+        }
+
+    }
 
 }

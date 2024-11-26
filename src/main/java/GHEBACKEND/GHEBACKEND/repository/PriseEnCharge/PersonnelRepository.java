bcc9609014bbb10c5438e76fb0b02b110d2b51ff
gaiusYan <gaiusyanbena11@gmail.com>
2024-11-26 15:04:38 +0100

    Les fonctionnalites de personnel

---
+++ src/main/java/GHEBACKEND/GHEBACKEND/repository/PriseEnCharge/PersonnelRepository.java
@@ -0,0 +1,6 @@
+package GHEBACKEND.GHEBACKEND.repository.PriseEnCharge;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import GHEBACKEND.GHEBACKEND.model.PriseEnCharge.PersonnelModel;
+
+public interface PersonnelRepository extends JpaRepository<PersonnelModel,Integer> {}

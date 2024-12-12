67e0b7d196bea309e17493e5dd317b76efbf3b41
YannickWnz <yannickwnz@gmail.com>
2024-12-12 20:45:59 +0100

    StudentInscriptionDetailsProjection

---
+++ src/main/java/GHEBACKEND/GHEBACKEND/model/PriseEnCharge/StudentInscriptionDetailsProjection.java
@@ -0,0 +1,11 @@
+package GHEBACKEND.GHEBACKEND.model.PriseEnCharge;
+
+public interface StudentInscriptionDetailsProjection {
+
+    String getAacLib();
+    String getClaLib();
+    String getProLib();
+    String getFilLib();
+    String getNivLib();
+
+}

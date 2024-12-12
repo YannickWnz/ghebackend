eed81d99be5d4cb61c4eb2b8b8abdbfbe8522c78
YannickWnz <yannickwnz@gmail.com>
2024-12-12 20:30:50 +0100

    method fetching inscription details

--- src/main/java/GHEBACKEND/GHEBACKEND/controller/Inscription/InscriptionController.java
+++ src/main/java/GHEBACKEND/GHEBACKEND/controller/Inscription/InscriptionController.java
@@ -1,25 +1,25 @@
 package GHEBACKEND.GHEBACKEND.controller.Inscription;
 
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-import org.springframework.web.bind.annotation.RequestParam;
-import GHEBACKEND.GHEBACKEND.model.Inscription.Inscription;
+import GHEBACKEND.GHEBACKEND.model.PriseEnCharge.StudentInscriptionDetailsProjection;
 import GHEBACKEND.GHEBACKEND.service.Inscription.InscriptionRequest;
 import GHEBACKEND.GHEBACKEND.service.Inscription.InscriptionRequestService;
 import GHEBACKEND.GHEBACKEND.service.Inscription.InscriptionService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 
 
 
@@ -36,7 +36,7 @@ public class InscriptionController {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(service.inscrire(request)) ;
         } catch (Exception ex) {
-            return ResponseEntity.ok(ex);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex);
         }
     }
 
@@ -51,6 +51,23 @@ public class InscriptionController {
             return ResponseEntity.status(HttpStatus.OK).body(inscriptionService.getInscriptionById(code));
         } catch (Exception ex) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex);
+        }
+    }
+
+    // Method by Wnz
+    @GetMapping("/details/{code}")
+    public ResponseEntity<?> getStudentInscriptionDetails(@PathVariable Integer code) {
+        try {
+        
+            List<StudentInscriptionDetailsProjection> data = inscriptionService.getStudentInscriptionDetails(code);
+            // Integer data = inscriptionService.getStudentInscriptionDetails(code);
+
+            return ResponseEntity.status(HttpStatus.OK).body(data);
+        
+        } catch (Exception ex) {
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex);
+        
         }
     }
     

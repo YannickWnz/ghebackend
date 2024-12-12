dcabfadefb483adb70712611f3d2f2a291711932
YannickWnz <yannickwnz@gmail.com>
2024-12-12 20:32:47 +0100

    new methods

--- src/main/java/GHEBACKEND/GHEBACKEND/service/PriseEnCharge/EtudiantService.java
+++ src/main/java/GHEBACKEND/GHEBACKEND/service/PriseEnCharge/EtudiantService.java
@@ -1,11 +1,13 @@
 package GHEBACKEND.GHEBACKEND.service.PriseEnCharge;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import GHEBACKEND.GHEBACKEND.model.PriseEnCharge.EtudiantModel;
+import GHEBACKEND.GHEBACKEND.model.PriseEnCharge.StudentInscriptionDetailsProjection;
 import GHEBACKEND.GHEBACKEND.repository.PriseEnCharge.EtudiantRepo;
 import GHEBACKEND.GHEBACKEND.utils.UtilityMethods;
 
@@ -22,6 +24,23 @@ public class EtudiantService {
     public List<EtudiantModel> getAllStudents() {
 
         return etudiantRepo.findAll();
+
+    }
+
+    public Optional<EtudiantModel> getStudentByCode(Integer code) {
+        
+        if (code == null) {
+            throw new IllegalArgumentException("Invalid student code");
+        }
+        return etudiantRepo.getStudentByCode(code);
+    }
+
+    public Optional<StudentInscriptionDetailsProjection> getStudentInscriptionDetails(Integer code) {
+
+        if(code == null) {
+            throw new IllegalArgumentException("Invalid student code");
+        }
+        return etudiantRepo.getStudentInscriptionDetails(code);
 
     }
 
